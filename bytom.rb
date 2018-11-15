7f1eddd55943af0b7b367f48a877343954594a71
Chengcheng Zhang <943420582@qq.com>
2018-11-15 16:55:38 +0800

    update

--- bytom.rb
+++ bytom.rb
@@ -4,7 +4,7 @@ class Bytom < Formula
     url 'https://github.com/bytom/bytom.git', :tag => 'v1.0.6rc1'
 
     devel do
-      url 'https://github.com/bytom/bytom.git', :branch => 'dev'
+      url 'https://github.com/bytom/bytom.git', :branch => 'master'
     end
   
     # Require El Capitan at least
