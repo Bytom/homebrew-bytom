c39b8bcb287df50cde13068ae81e84bbf194d920
Chengcheng Zhang <943420582@qq.com>
2018-11-16 00:52:06 +0800

    update

--- bytom.rb
+++ bytom.rb
@@ -1,31 +1,25 @@
 class Bytom < Formula
-    desc "A digital asset layer protocol is the infrastructure of asset Internet."
+    desc "Official Go implementation of the Bytom protocol "
     homepage "https://bytom.io/"
-    url 'https://github.com/bytom/bytom.git', :tag => 'v1.0.6rc1'
+    url "https://github.com/Bytom/bytom/releases/download/v1.0.6rc1/bytom-1.0.6rc1-darwin_amd64.tgz"
     sha256 "bd4abc64860a05b5dd30b0d1579b3c9d58f3179532a9c154f9ac83364fb88699"
-
-    devel do
-        url 'https://github.com/bytom/bytom.git', :branch => 'dev'
-    end
-
-    # Require El Capitan at least
-    depends_on :macos => :el_capitan
-
-    # Is there a better way to ensure that frameworks (IOKit, CoreServices, etc) are installed?
-    depends_on :xcode => :build
-    
-    depends_on 'go' => :build
   
     def install
-        # ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
-        system "go", "env" # Debug env
-        system "make", "bytomd"
-        system "make", "bytomcli"
-        bin.install 'cmd/bytomd/bytomd'
-        bin.install 'cmd/bytomd/bytomcli'
+      bin.install "bytomd-darwin_amd64"
+      bin.install "bytomcli-darwin_amd64"
     end
   
     test do
-        system "bytomcli", "version"
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test bytom`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
     end
   end
+  
