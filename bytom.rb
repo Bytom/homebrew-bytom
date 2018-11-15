4946235f55b6475e5fd53b5f8de74427cdc27df5
Chengcheng Zhang <943420582@qq.com>
2018-11-15 18:39:40 +0800

    update

--- bytom.rb
+++ bytom.rb
@@ -1,30 +1,31 @@
 class Bytom < Formula
     desc "A digital asset layer protocol is the infrastructure of asset Internet."
-    homepage 'https://bytom.io/'
+    homepage "https://bytom.io/"
     url 'https://github.com/bytom/bytom.git', :tag => 'v1.0.6rc1'
+    sha256 "bd4abc64860a05b5dd30b0d1579b3c9d58f3179532a9c154f9ac83364fb88699"
 
     devel do
-      url 'https://github.com/bytom/bytom.git', :branch => 'master'
+        url 'https://github.com/bytom/bytom.git', :branch => 'dev'
     end
-  
+
     # Require El Capitan at least
     depends_on :macos => :el_capitan
-  
+
     # Is there a better way to ensure that frameworks (IOKit, CoreServices, etc) are installed?
     depends_on :xcode => :build
-  
+    
     depends_on 'go' => :build
   
     def install
-    #   ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
-      system "go", "env" # Debug env
-      system "make", "bytomd"
-      system "make", "bytomcli"
-      bin.install 'cmd/bytomd/bytomd'
-      bin.install 'cmd/bytomd/bytomcli'
+        # ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
+        system "go", "env" # Debug env
+        system "make", "bytomd"
+        system "make", "bytomcli"
+        bin.install 'cmd/bytomd/bytomd'
+        bin.install 'cmd/bytomd/bytomcli'
     end
-
+  
     test do
         system "bytomcli", "version"
     end
-end
+  end
